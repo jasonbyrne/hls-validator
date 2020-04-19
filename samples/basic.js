8f34b84c7bdea11a0ee9f7500f7ea273e2af4d04
Jason Byrne <jason.byrne@flocasts.com>
2020-04-18 22:44:52 -0400

    just building out some basics

--- samples/basic.js
+++ samples/basic.js
@@ -1,12 +1,18 @@
 const hls = require("../dist/index");
+const msv = require("../dist/mediastreamvalidator");
+
+const bitMovinSample =
+  "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8";
+const jwPlayerSample = "https://content.jwplatform.com/manifests/yp34SRmf.m3u8";
+const floWrestling =
+  "https://damb2tknfsomm.cloudfront.net/uploaded/KRVpJG0KgRPe2dAzL0MPge4gz520vqyM/playlist.m3u8";
 
 (async () => {
-  const stream = await hls.Stream.load(
-    "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
-  );
+  const stream = await hls.Stream.load(floWrestling);
   if (stream.validate()) {
     console.log("This is a valid stream.");
   } else {
     console.log(`Invalid stream. Following issues noted: ${stream.errors}`);
   }
+  console.log(stream);
 })();
--- src/stream.ts
+++ src/stream.ts
@@ -8,28 +8,69 @@ HLS.setOptions({
 });
 
 export class Stream {
-  private _manifest: HLS.types.MasterPlaylist | HLS.types.MediaPlaylist | null;
+  private _uri: string;
+  private _masterPlaylist: HLS.types.MasterPlaylist | null;
+  private _mediaPlaylists: HLS.types.MediaPlaylist[] = [];
   private _errors: string[] = [];
+
+  public get masterPlaylist(): HLS.types.MasterPlaylist | null {
+    return this._masterPlaylist;
+  }
+
+  public get mediaPlaylists(): HLS.types.MediaPlaylist[] {
+    return this._mediaPlaylists;
+  }
 
   public get errors(): string[] {
     return this._errors;
   }
 
   public static async load(uri: string): Promise<Stream> {
-    return new Stream(await getManifest(uri));
+    const stream = new Stream(uri, await getManifest(uri));
+    await stream._loadMediaPlaylists();
+    return stream;
   }
 
-  private constructor(source: string) {
+  private constructor(uri: string, source: string) {
+    this._uri = uri;
     try {
-      this._manifest = HLS.parse(source);
+      const manifest:
+        | HLS.types.MasterPlaylist
+        | HLS.types.MediaPlaylist = HLS.parse(source);
+      if (manifest.isMasterPlaylist) {
+        this._masterPlaylist = <HLS.types.MasterPlaylist>manifest;
+        this._masterPlaylist.uri = uri;
+      } else {
+        this._masterPlaylist = null;
+        this._mediaPlaylists.push(<HLS.types.MediaPlaylist>manifest);
+      }
     } catch (ex) {
       this._errors.push(ex.message);
-      this._manifest = null;
+      this._masterPlaylist = null;
+    }
+  }
+
+  private async _loadMediaPlaylists() {
+    if (this._masterPlaylist !== null) {
+      const promises: Promise<void>[] = [];
+      this._masterPlaylist.variants.forEach(async (variant) => {
+        promises.push(
+          new Promise(async (resolve) => {
+            const url = new URL(variant.uri, this._uri);
+            const content = await getManifest(url.href);
+            this._mediaPlaylists.push(
+              <HLS.types.MediaPlaylist>HLS.parse(content)
+            );
+            resolve();
+          })
+        );
+      });
+      await Promise.all(promises);
     }
   }
 
   public validate(): boolean {
-    if (this._manifest !== null) {
+    if (this._masterPlaylist !== null || this._mediaPlaylists.length > 0) {
       return true;
     }
     return false;
